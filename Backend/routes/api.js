024d7775c7922f74ddb5df8cba1f4f2bc13c8add
Kishan Kulkarni <124907215+Lars1156@users.noreply.github.com>
2024-08-30 15:17:04 +0530

    creating the project creation

--- Backend/routes/api.js
+++ Backend/routes/api.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controller/userController');
+const projectController = require('../controller/projectController');
 
 router.post('/loginUser', userController.loginUser);
+
+// Routes of Project Registration 
+router.post('/createProject' , projectController.createProjects);
 
 module.exports = router;
